8a50176ba9e9b3ca79bcea7306320b12fb275dde
rwijtvliet <rwijtvliet@gmail.com>
2023-04-05 22:57:51 +0200

    initial commit

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,24 @@
 return {
   -- first key is the mode
   n = {
+    -- n, t, s: dvorak navigation
+    ["n"] = { "v:count ? 'j' : 'gj'", expr = true, desc = "Move cursor down" },
+    ["t"] = { "v:count ? 'k' : 'gk'", expr = true, desc = "Move cursor up" },
+    ["s"] = { "l", desc = "Move cursor right"},
+    -- N, T, S: 
+    ["S"] = { "L", desc = "Move to screen bottom"},
+    -- TODO: T and N and Control+...
+
+    -- j, k, l: remap to default actions for n, t, s
+    ["l"] = { "t", desc = "Jump to before next occurance of character"}, 
+    ["L"] = { "T", desc = "Jump to after prev occurance of character"},
+
+    ["k"] = { "s", desc = "Delete character and substitute text"},
+    ["K"] = { "S", desc = "Delete line and substitute text"},
+
+    ["j"] = { "n", desc = "Repeat search forward"},
+    ["J"] = { "N", desc = "Repeat search backward"},
+    
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
---
+++ remap.lua
@@ -0,0 +1,12 @@
+print("hallo2")
+
+vim.keymap.set("n", "l", "s")
+vim.keymap.set("n", "s", "l")
+
+vim.keymap.set("n", "j", "n")
+vim.keymap.set("n", "n", "j")
+
+vim.keymap.set("n", "k", "t")
+vim.keymap.set("n", "t", "k")
+
+
