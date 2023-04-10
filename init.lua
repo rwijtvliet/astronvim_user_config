e68f3e48b8f51f4144eae4c573aec6b462283d1c
rwijtvliet <rwijtvliet@gmail.com>
2023-04-10 23:06:55 +0200

    navigational not working (s)

--- init.lua
+++ init.lua
@@ -1,3 +1,38 @@
+local utils = require "astronvim.utils"
+local is_available = utils.is_available
+-- Remappings that work everywhere
+---- navigation
+vim.api.nvim_set_keymap("", "t", "k", {desc = "Move cursor up"})
+vim.api.nvim_set_keymap("", "n", "j", {desc = "Move cursor dwn"})
+vim.api.nvim_set_keymap("", "s", "l", {desc = "Move cursor rght"})
+---- capitals of navigation
+vim.api.nvim_set_keymap("", "T", "K", {desc = "Help"})
+vim.api.nvim_set_keymap("", "S", "L", {desc = "Move cursor to bottom of screen"})
+---- reverse mappings
+vim.api.nvim_set_keymap("", "l", "n", {desc = "Next search result"})
+vim.api.nvim_set_keymap("", "L", "N", {desc = "Prev search result"})
+vim.api.nvim_set_keymap("", "j", "s", {desc = "Substitute character"})
+vim.api.nvim_set_keymap("", "k", "t", {desc = "Till (forward)"})
+vim.api.nvim_set_keymap("", "K", "T", {desc = "Till (backward)"})
+-- Remappings that work in normal mode
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", {desc = "Move to buffer on left"})
+vim.api.nvim_set_keymap("n", "<C-t>", "<C-w>k", {desc = "Move to buffer above"})
+vim.api.nvim_set_keymap("n", "<C-n>", "<C-w>j", {desc = "Move to buffer below"})
+vim.api.nvim_set_keymap("n", "<C-s>", "<C-w>l", {desc = "Move to buffer on right"})
+-- if is_available "smart-splits.nvim" then
+--   vim.api.nvim_set_keymap("n", "<C-h>", {function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" })
+--   vim.api.nvim_set_keymap("n", "<C-n>", {function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" })
+--   vim.api.nvim_set_keymap("n", "<C-t>", { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" })
+--   vim.api.nvim_set_keymap("n", "<C-s>", { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" })
+-- else
+--   vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>j", { desc = "Move to left split" })
+--   vim.api.nvim_set_keymap("n", "<C-n>", "<C-w>j", { desc = "Move to below split" })
+--   vim.api.nvim_set_keymap("n", "<C-t>", "<C-w>k", { desc = "Move to above split" })
+--   vim.api.nvim_set_keymap("n", "<C-s>", "<C-w>l", { desc = "Move to right split" })
+-- end
+
+vim.api.nvim_set_option("equalalways", true) -- make windows same width when closing one
+
 return {
   -- Configure AstroNvim updates
   updater = {
@@ -67,6 +102,7 @@ return {
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
+  --
   polish = function()
     -- Set up custom filetypes
     -- vim.filetype.add {
@@ -82,3 +118,5 @@ return {
     -- }
   end,
 }
+
+
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,19 @@
 return {
+  {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end},
+  {
+    "max397574/better-escape.nvim",
+    opts = { timeout = 300, mapping = {"hh", "ht"},  }
+  }
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
